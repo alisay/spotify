cb2675cdc0e5f33255131b1e0e9025f134db87a2
Brandon Robertson <42512857+brandon-robertson-dev@users.noreply.github.com>
2020-09-21 14:29:45 +1000

    added code for itunes (#4)
    * changes
    
    * added apple thingo

--- controller.js
+++ controller.js
@@ -1,17 +1,28 @@
 const fetch = require('node-fetch');
+const open = require('open');
 
 function getSongQuery(query) {
     fetch("https://searchly.asuarez.dev/api/v1/song/search?query=" + query) //QUERY CAN BE ANYTHING
         .then(response => response.json())
-        .then(data => console.log(data.response.results))
+        .then(data => {
+            let randomNum = Math.floor(Math.random() * (data.response.results.length))
+            getAppleJson(data.response.results[randomNum].name.split(' ').join('+'))
+        })
         .catch(err => console.log(err));
 }
 
-function getSongId(songId) {
-    fetch("https://searchly.asuarez.dev/api/v1/similarity/by_song?song_id=" + songId) //SONG ID NEEDED
+// function getSongId(songId) {
+//     fetch("https://searchly.asuarez.dev/api/v1/similarity/by_song?song_id=" + songId) //SONG ID NEEDED
+//         .then(response => response.json())
+//         .then(data => console.log(data.response.similarity_list))
+//         .catch(err => console.log(err));
+// }
+
+function getAppleJson(song) {
+    fetch("https://itunes.apple.com/search?term=" + song + "&entity=song")
         .then(response => response.json())
-        .then(data => console.log(data.response.similarity_list))
-        .catch(err => console.log(err));
+        .then(data => open(data.results[0].trackViewUrl))
+        .catch(err => console.log(err))
 }
-getSongQuery('')
-getSongId()
+
+getSongQuery('happy')

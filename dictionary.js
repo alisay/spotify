0eaa3ebf5b8dae770fd500b5d6ccf2c6794870c6
Alisa <alisa.blakeney@gmail.com>
2020-09-21 14:40:22 +0800

    update dictionary api call to handle no result error

--- dictionary.js
+++ dictionary.js
@@ -5,12 +5,21 @@ const antonymFinder = (word)=>{
     const thesaurus = new Backend();
     thesaurus.setBaseUrl("https://www.dictionaryapi.com/api/v3/references/thesaurus/json")
     return thesaurus.get(`/${word}?key=${key.api}`)
-    .then(data=> console.log(data[0]["meta"]["ants"]))
+    .then(data=> data[0]["meta"]["ants"])
+    .then(data=>showAntonyms(data))
     .catch(console.log);
 }
 
-// const showAntonyms = ()=>{
+const showAntonyms = (data)=>{
+    try{
+        if(data.length<1){
+        throw new Error("no antonyms")
+    }
+    console.log(data[0][0]);
+}
+    catch(err){
+        console.log(err);
+    }
+}
 
-// }
-
-console.log(antonymFinder("happy"));
+antonymFinder("miserable");

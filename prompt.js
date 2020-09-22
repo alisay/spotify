90559f743386b1fc9f80e4f043f97d77babd76e0
Alisa <alisa.blakeney@gmail.com>
2020-09-22 14:07:27 +0800

    fix ability to select 2 options, thanks B

--- prompt.js
+++ prompt.js
@@ -16,7 +16,7 @@ const ask = () =>{
         },
         {
           name: 'preferred-outcome',
-          type: 'checkbox',
+          type: 'list',
           message: 'Do you want to feel the opposite or stay the same?',
           choices: ["same", "different"],
           validate: function(value) {
